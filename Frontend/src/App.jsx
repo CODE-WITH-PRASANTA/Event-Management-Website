ec5330e359f0c784e2dc332bc313ef4ac3ecaeca
Ankitanayak28 <153258452+Ankitanayak28@users.noreply.github.com>
2026-02-24 11:31:06 +0530

    pull

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import AboutUsHome from './Components/AboutUsHome/AboutUsHome'
 
 function App() {
 
   return (
   <>
-  
+  <Routes>
+    <Route path="/AboutUs\Home"element={<AboutUsHome/>}/>
+  </Routes>
   </>
   )
 }
