3b79deb4e2e368fd13d38937bfe10aebe01e83ea
santanuranjanbal-jpg <santanuranjanbal@gmail.com>
2026-03-02 17:13:38 +0530

    Done

--- Frontend/src/Pages/RSVP/RSVP.jsx
+++ Frontend/src/Pages/RSVP/RSVP.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import RSVPBreadcrum from '../../Components/RSVPBreadcrum/RSVPBreadcrum'
+import RSVPInvitation from '../../Components/RSVPInvitation/RSVPInvitation'
+import RSVPWedingHero from '../../Components/RSVPWedingHero/RSVPWedingHero'
+import RSVPPartner from '../../Components/RSVPPartner/RSVPPartner'
 
 const RSVP = () => {
   return (
     <div>
       <RSVPBreadcrum />
+      <RSVPInvitation />
+     <RSVPWedingHero />
+     <RSVPPartner />
     </div>
   )
 }
